d42d39578e5a3b84909a024fabf9174b1df49b10
StarGazer1995 <gz19951230@outlook.com>
2020-11-26 22:28:20 +0800

    update time functions

--- cpp/headers/parameters.h
+++ cpp/headers/parameters.h
@@ -8,17 +8,39 @@
 #endif //SPECIALCOURSE_PARAMETERS_H
 #include <opencv2/opencv.hpp>
 #include <vector>
-
+#include <sys/time.h>
 using namespace cv;
 using namespace std;
-class dataclaster {
-public:
-    struct MeasuredData {
-        vector<cv::Vec3d> R;
-        vector<cv::Vec3d> T;
-    };
-};
+
 struct MeasuredData {
     vector<cv::Vec3d> R;
     vector<cv::Vec3d> T;
 };
+static string GetLocalTimeWithMs(void)
+{
+    string defaultTime = "19700101000000000";
+    try
+    {
+        struct timeval curTime;
+        gettimeofday(&curTime, NULL);
+        int milli = curTime.tv_usec / 1000;
+
+        char buffer[80] = {0};
+        struct tm nowTime;
+        localtime_r(&curTime.tv_sec, &nowTime);//把得到的值存入临时分配的内存中，线程安全
+        strftime(buffer, sizeof(buffer), "%Y%m%d%H%M%S", &nowTime);
+
+        char currentTime[84] = {0};
+        snprintf(currentTime, sizeof(currentTime), "%s%03d", buffer, milli);
+
+        return currentTime;
+    }
+    catch(const std::exception& e)
+    {
+        return defaultTime;
+    }
+    catch (...)
+    {
+        return defaultTime;
+    }
+}
